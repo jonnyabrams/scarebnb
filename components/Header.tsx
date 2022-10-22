01a5c51e57797c3341e6b380a76694043e533381
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-22 10:17:08 +0100

    styles: searchbar text

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
 
         {/* middle - search */}
         <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-          <input className="flex-grow pl-5 bg-transparent outline-none" type="text" placeholder="Start your search" />
+          <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search" />
           <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
         </div>
 
