7356e151b720f6dfcedf77abd168ecacbdb51ace
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-23 08:30:53 +0100

    feat: right side of Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { MagnifyingGlassIcon, GlobeAltIcon, Bars3Icon, UserCircleIcon, UsersIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
   return (
     <div>
-      <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      <header className="sticky top-0 z-50 grid grid-cols-3 p-5 bg-white shadow-md md:px-10">
         {/* left side */}
-        <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <div className="relative flex items-center h-10 my-auto cursor-pointer">
           <Image
             src="/sparebnb-logo.png"
             layout="fill"
@@ -17,13 +17,21 @@ const Header = () => {
         </div>
 
         {/* middle - search */}
-        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-          <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search" />
-          <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+        <div className="flex items-center py-2 rounded-full md:border-2 md:shadow-sm">
+          <input className="flex-grow pl-5 text-sm text-gray-600 placeholder-gray-400 bg-transparent outline-none" type="text" placeholder="Start your search" />
+          <MagnifyingGlassIcon className="hidden h-8 p-2 text-white bg-red-400 rounded-full cursor-pointer md:inline-flex md:mx-2" />
         </div>
 
         {/* right side */}
-        <div></div>
+        <div className="flex items-center justify-end space-x-4 text-gray-500">
+          <p className="hidden cursor-pointer md:inline">Become a host</p>
+          <GlobeAltIcon className="h-6 cursor-pointer" />
+
+          <div className="flex items-center p-2 space-x-2 border-2 rounded-full">
+            <Bars3Icon className="h-6" />
+            <UserCircleIcon className="h-6" />
+          </div>
+        </div>
       </header>
     </div>
   );
