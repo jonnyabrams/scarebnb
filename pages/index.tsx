5d410f4ba71c96fccbc2207f77578316dd6fbcfd
jonnyabrams <jonnyabrams@hotmail.com>
2022-10-24 08:39:39 +0100

    feat: create main section in Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
-
+import type { NextPage } from "next";
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
@@ -15,8 +14,16 @@ const Home: NextPage = () => {
 
       <Header />
       <Banner />
-    </div>
-  )
-}
 
-export default Home
+      <main className="px-8 mx-auto max-w-7xl sm:px-16">
+        <section className="pt-6">
+          <h2 className="pb-5 text-4xl font-semibold">Explore Nearby</h2>
+
+          
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
