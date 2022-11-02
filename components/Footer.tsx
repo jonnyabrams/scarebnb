dd87c782f274dfa239db067e3df87261344c9086
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-02 14:39:12 +0000

    feat: Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,43 @@
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 px-32 text-gray-600 bg-gray-100 gap-y-10 md:grid-cols-4 py-14">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How ScareBnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>ScareBnb Plus</p>
+        <p>ScareBnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It is a clone</p>
+        <p>Of AirBnb</p>
+        <p>Why are you reading this?</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi YouTube</p>
+        <p>Easter Eggs</p>
+        <p>For the Win</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">OTHER STUFF</h5>
+        <p>Stop reading this</p>
+        <p>Seriously, stop</p>
+        <p>Why are you not listening?</p>
+        <p>Cut it out</p>
+        <p>Fine, have it your way</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import LargeCard from "../components/LargeCard";
 import MediumCard from "../components/MediumCard";
@@ -51,6 +52,7 @@ const Home = ({ exploreData, cardsData }: IProps) => {
           buttonText="Get Inspired"
         />
       </main>
+      <Footer />
     </div>
   );
 };
