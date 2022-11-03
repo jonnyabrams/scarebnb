93c3e942ad199dced7fa1e921871846190983292
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-03 07:36:33 +0000

    feat: pull in search data in Search & format dates

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,16 +1,28 @@
+import { useRouter } from "next/router";
+import moment from "moment";
+
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const Search = () => {
+  const router = useRouter();
+  const { location, startDate, endDate, noOfGuests } = router.query;
+
+  const formattedStartDate = moment(startDate).format('DD/MM/YYYY');
+  const formattedEndDate = moment(endDate).format('DD/MM/YYYY');
+  const range = `${formattedStartDate} - ${formattedEndDate}`
+
   return (
     <div>
       <Header />
 
       <main className="flex">
         <section className="flex-grow px-6 pt-14">
-          <p className="text-xs">300+ Stays for 5 guests</p>
+          <p className="text-xs">300+ Stays - {range} for {noOfGuests} guests</p>
 
-          <h1 className="mt-2 mb-6 text-3xl font-semibold">Stays in Mars</h1>
+          <h1 className="mt-2 mb-6 text-3xl font-semibold">
+            Stays in {location}
+          </h1>
 
           <div className="hidden mb-5 space-x-3 text-gray-800 lg:inline-flex whitespace-nowrap">
             <p className="button">Cancellation Flexibility</p>
