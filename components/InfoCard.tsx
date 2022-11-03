8063c64e13af94d782decd35e36f21e52af45dad
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-03 10:20:42 +0000

    styles: add border radius to InfoCard img

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -16,7 +16,7 @@ const InfoCard = ({
   return (
     <div className="flex px-2 pr-4 transition duration-200 ease-out border-b cursor-pointer py-7 hover:opacity-80 hover:shadow-lg first:border-t">
       <div className="relative flex-shrink-0 w-40 h-24 md:h-52 md:w-80">
-        <Image src={img} layout="fill" objectFit="cover" alt="Property" />
+        <Image src={img} layout="fill" objectFit="cover" alt="Property" className="rounded-2xl" />
       </div>
 
       <div className="flex flex-col flex-grow pl-5">
